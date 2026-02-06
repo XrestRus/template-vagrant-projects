e6a0378a3bea929fa33eb42975d96d0a4f5b18fe
Nikita <xrestrus@gmail.com>
2026-02-06 20:05:32 +0700

    Доработка

--- project-vb-docker/run.ps1
+++ project-vb-docker/run.ps1
@@ -7,9 +7,12 @@
 
 $Env:VAGRANT_PREFER_SYSTEM_BIN = 0
 
-vagrant up
+vagrant up --provider=virtualbox
 
 vagrant upload .env /home/vagrant/.env
 vagrant upload run.sh /home/vagrant/run.sh
+
+# Конвертируем окончания строк Windows -> Unix
+vagrant ssh -c "dos2unix /home/vagrant/.env /home/vagrant/run.sh 2>/dev/null || sed -i 's/\r$//' /home/vagrant/.env /home/vagrant/run.sh"
 
 vagrant ssh -- -t "bash /home/vagrant/run.sh"
